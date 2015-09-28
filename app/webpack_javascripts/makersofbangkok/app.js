1aeb6419acbc839f558b475ff12e6f9ae1697ef5
Jan Klimo <jan.klimo@gmail.com>
2015-09-28 23:42:37 +0700

    landing page ready

--- app/webpack_javascripts/makersofbangkok/app.js
+++ app/webpack_javascripts/makersofbangkok/app.js
@@ -6,6 +6,34 @@ const LandingPage = React.createClass({
   render() {
     let windowHeight = $(window).height();
     return <section id="home" style={{height: windowHeight}}>
+      <div className="container">
+        <div className="row">
+          <div className="logo-container text-center">
+            <img id="logo" src="assets/logo.png" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="taglines text-center">
+            <h2>We are</h2>
+            <h1>Makers of Bangkok</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="intro text-center">
+            <p>We're entrepreneurs, programmers, designers,
+               photographers, event organizers.<br />
+               What we have in common is our passion for building,
+               creating, and making things happen.
+               <br /><br />
+               Sounds like you?</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="cta text-center">
+            <a href="#" className="btn btn-main">Join Us!</a>
+          </div>
+        </div>
+      </div>
     </section>;
   }
 });
