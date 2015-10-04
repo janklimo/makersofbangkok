96074cfb1f3fca64f1db90413883f0409c009383
Jan Klimo <jan.klimo@gmail.com>
2015-10-04 15:53:44 +0700

    fix React version, include LinkedStateMixin

--- app/webpack_javascripts/makersofbangkok/components/public/signup_modal.jsx
+++ app/webpack_javascripts/makersofbangkok/components/public/signup_modal.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap/lib';
+import LinkedStateMixin from 'react-addons-linked-state-mixin';
+import { Modal, Button, Input } from 'react-bootstrap/lib';
 
 export default React.createClass({
+  mixins: [ LinkedStateMixin ],
+
+  getInitialState() {
+    return { referrerEmail: '' };
+  },
+
   render() {
+    const innerButton = <Button>Verify</Button>;
     return <Modal show={this.props.show} onHide={this.props.onHide}>
       <Modal.Header closeButton>
         <Modal.Title>You&#39;re here!</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        Nehehe
+        <p>Makers of Bangkok is invitation-only.<br /></p>
+        <Input type="text"
+          value={this.state.value}
+          placeholder="Their email address"
+          label="Who's the cool person who invited you?"
+          groupClassName="group-class"
+          labelClassName="label-class"
+          valueLink={this.linkState('referrerEmail')}
+          buttonAfter={innerButton} />
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={this.props.onHide}>Close</Button>
