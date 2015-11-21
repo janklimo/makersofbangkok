2ba6e6446915b084fa8c707816a9bef3336de7c5
Jan Klimo <jan.klimo@gmail.com>
2015-11-21 19:46:16 +0700

    fix loader during sign in

--- app/webpack_javascripts/makersofbangkok/stores/auth.jsx
+++ app/webpack_javascripts/makersofbangkok/stores/auth.jsx
@@ -2,6 +2,7 @@ import Reflux from 'reflux';
 import request from 'superagent';
 import AuthActions from '../actions/auth';
 import history from '../utils/history';
+import LoaderActions from '../actions/loader';
 
 export default Reflux.createStore({
   listenables: [ AuthActions ],
@@ -11,8 +12,10 @@ export default Reflux.createStore({
   },
 
   onLogin(user) {
+    LoaderActions.load();
     request.post('/api/v1/users/sign_in')
     .send(user).end((err, res) => {
+      LoaderActions.load.completed();
       if (err) {
         AuthActions.login.failed(err);
       } else {
