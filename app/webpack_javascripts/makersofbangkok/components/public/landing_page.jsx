bcd2d063ad47b3ee21e9f8fc3481340d6e3efff2
Jan Klimo <jan.klimo@gmail.com>
2016-03-03 22:07:20 +0700

    what it's about ready

--- app/webpack_javascripts/makersofbangkok/components/public/landing_page.jsx
+++ app/webpack_javascripts/makersofbangkok/components/public/landing_page.jsx
@@ -78,15 +78,43 @@ export default React.createClass({
               <div className="section-subheader">
                 <i className="fa fa-envelope"></i>
               </div>
+              <div className="section-title">
+                Invitation Only
+              </div>
+              <div className="section-text">
+                Membership is not open to public. We leave it up to our users
+                to invite people they like and respect. Inviting a new member
+                is a sign of trust that it's a person other members will
+                benefit from meeting.
+              </div>
             </div>
             <div className="col-sm-4">
               <div className="section-subheader">
                 <i className="fa fa-users"></i>
               </div>
+              <div className="section-title">
+                Small Groups
+              </div>
+              <div className="section-text">
+                Attendance is normally limited to 10 people. This way,
+                everybody gets a chance to meet and connect with all the other
+                attendees.
+              </div>
             </div>
             <div className="col-sm-4">
               <div className="section-subheader">
                 <i className="fa fa-lightbulb-o"></i>
+              </div>
+              <div className="section-title">
+                Meaningful Conversations
+              </div>
+              <div className="section-text">
+                While meetups have no formal structure, we do make sure that
+                everybody gets a chance to introduce themselves, speak about
+                what they're excited about, tell others about the problems
+                they are trying to solve, or need help with. Get instant
+                feedback, ideas, intros = the most value out of one evening
+                possible.
               </div>
             </div>
           </div>
